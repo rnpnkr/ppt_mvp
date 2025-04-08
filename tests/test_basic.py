fa3f14ce650bf7d69f2b6b2d2764c292d24ec3fb
rnpnkr <aryan.panhekar@gmail.com>
2025-04-08 10:25:33 +0530

    Delete tests directory
    not needed

--- tests/test_basic.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-class TestBasic(unittest.TestCase):
-    def test_placeholder(self):
-        self.assertEqual(1, 1)  # Dummy test that always passes
-
-if __name__ == '__main__':
-    unittest.main()
